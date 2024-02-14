7c17c0d8759ef39a9ba3be110858de8443b49783
Usman Cheema <140833929+Usman-cheema79@users.noreply.github.com>
2024-02-14 16:52:58 +0500

    contact form

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -11,6 +11,24 @@ export default function Contact() {
     <>
           <Navbar ccolour={navlicolour}/>
           <Header title={title} pagename={pagename}/>
+           <div className={Contactcss.contactpagesaction}>
+                <div className={Contactcss.contactpageinfo}>
+                      <button className={Contactcss.btn1}>CONTACT US</button>
+                      <h1>Get In Touch With FourDevs</h1>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius tempor incididunt ut labore et dolore magna aliqua. Ut enim adiqua minim veniam quis nostrud exercitation ullamco</p>
+                    </div>
+                <div className={Contactcss.contactpageform}>
+                      <form>
+                           <input type="text" className={Contactcss.forminput} placeholder='Your Name' />
+                           <input type="text" className={Contactcss.forminput} placeholder='Your Email' />
+                           <input type="text" className={Contactcss.forminput} placeholder='Your Subject' />
+                           <textarea id="paragraphInput" name="paragraphInput" className={Contactcss.forminput} rows="5" placeholder='Your Massage'></textarea>
+                           <div className={Contactcss.formbtn}>
+                             <h className="footer-subscribe-btntext"> Send Massage</h>
+                          </div>
+                      </form>
+                </div>
+           </div>
           <Footer/>
     </>
   )
